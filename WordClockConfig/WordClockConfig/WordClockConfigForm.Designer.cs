1b81111532b9cfdb273dfa67352af05359b4c3d2
Robert Chipperfield <robert@syxis.co.uk>
2019-03-02 20:41:19 +0000

    Basic WordClock configuration application

---
+++ WordClockConfig/WordClockConfig/WordClockConfigForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WordClockConfig
+{
+    partial class WordClockConfigForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label3;
+            this.m_SaveButton = new System.Windows.Forms.Button();
+            this.m_PortPicker = new System.Windows.Forms.ComboBox();
+            this.m_DatePicker = new System.Windows.Forms.DateTimePicker();
+            this.m_TimePicker = new System.Windows.Forms.DateTimePicker();
+            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(label2, 0, 1);
+            tableLayoutPanel1.Controls.Add(label3, 0, 2);
+            tableLayoutPanel1.Controls.Add(this.m_SaveButton, 1, 3);
+            tableLayoutPanel1.Controls.Add(this.m_PortPicker, 1, 0);
+            tableLayoutPanel1.Controls.Add(this.m_DatePicker, 1, 1);
+            tableLayoutPanel1.Controls.Add(this.m_TimePicker, 1, 2);
+            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            tableLayoutPanel1.Size = new System.Drawing.Size(337, 197);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(3, 18);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(57, 13);
+            label1.TabIndex = 0;
+            label1.Text = "Serial port:";
+            // 
+            // label2
+            // 
+            label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(3, 67);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(57, 13);
+            label2.TabIndex = 1;
+            label2.Text = "Date:";
+            // 
+            // label3
+            // 
+            label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(3, 116);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(57, 13);
+            label3.TabIndex = 2;
+            label3.Text = "Time:";
+            // 
+            // m_SaveButton
+            // 
+            this.m_SaveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_SaveButton.Location = new System.Drawing.Point(66, 160);
+            this.m_SaveButton.Name = "m_SaveButton";
+            this.m_SaveButton.Size = new System.Drawing.Size(268, 23);
+            this.m_SaveButton.TabIndex = 3;
+            this.m_SaveButton.Text = "&Save Configuration";
+            this.m_SaveButton.UseVisualStyleBackColor = true;
+            this.m_SaveButton.Click += new System.EventHandler(this.SaveConfigurationClicked);
+            // 
+            // m_PortPicker
+            // 
+            this.m_PortPicker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_PortPicker.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.m_PortPicker.FormattingEnabled = true;
+            this.m_PortPicker.Location = new System.Drawing.Point(66, 14);
+            this.m_PortPicker.Name = "m_PortPicker";
+            this.m_PortPicker.Size = new System.Drawing.Size(268, 21);
+            this.m_PortPicker.TabIndex = 4;
+            // 
+            // m_DatePicker
+            // 
+            this.m_DatePicker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_DatePicker.Location = new System.Drawing.Point(66, 63);
+            this.m_DatePicker.Name = "m_DatePicker";
+            this.m_DatePicker.Size = new System.Drawing.Size(268, 20);
+            this.m_DatePicker.TabIndex = 5;
+            this.m_DatePicker.ValueChanged += new System.EventHandler(this.TimeChanged);
+            // 
+            // m_TimePicker
+            // 
+            this.m_TimePicker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_TimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.m_TimePicker.Location = new System.Drawing.Point(66, 112);
+            this.m_TimePicker.Name = "m_TimePicker";
+            this.m_TimePicker.ShowUpDown = true;
+            this.m_TimePicker.Size = new System.Drawing.Size(268, 20);
+            this.m_TimePicker.TabIndex = 6;
+            this.m_TimePicker.ValueChanged += new System.EventHandler(this.TimeChanged);
+            // 
+            // WordClockConfigForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 197);
+            this.Controls.Add(tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.Name = "WordClockConfigForm";
+            this.Text = "WordClock Configuration";
+            this.Load += new System.EventHandler(this.WordClockConfigForm_Load);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button m_SaveButton;
+        private System.Windows.Forms.ComboBox m_PortPicker;
+        private System.Windows.Forms.DateTimePicker m_DatePicker;
+        private System.Windows.Forms.DateTimePicker m_TimePicker;
+    }
+}
+
---
+++ WordClockConfig/WordClockConfig/WordClockConfigForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordClockConfig
+{
+    public partial class WordClockConfigForm : Form
+    {
+        private System.Windows.Forms.Timer m_UpdateTimeTimer;
+        private bool m_TimeSetManually;
+
+        public WordClockConfigForm()
+        {
+            InitializeComponent();
+        }
+
+        private void WordClockConfigForm_Load(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(PopulateSerialPorts);
+
+            m_UpdateTimeTimer = new System.Windows.Forms.Timer();
+            m_UpdateTimeTimer.Interval = 1000;
+            m_UpdateTimeTimer.Tick += UpdateTime;
+            m_UpdateTimeTimer.Enabled = true;
+        }
+
+        private void UpdateTime(object sender, EventArgs e)
+        {
+            if (Disposing || IsDisposed)
+                return;
+
+            if (m_TimeSetManually)
+            {
+                m_UpdateTimeTimer.Enabled = false;
+            }
+            else
+            {
+                m_DatePicker.Value = DateTime.Now;
+                m_TimePicker.Value = DateTime.Now;
+                m_TimeSetManually = false;
+            }
+        }
+
+        private void PopulateSerialPorts()
+        {
+            var ports = SerialPort.GetPortNames();
+            Invoke(new MethodInvoker(() =>
+            {
+                if (Disposing || IsDisposed)
+                    return;
+                m_PortPicker.Items.Clear();
+                Array.ForEach(ports, port => m_PortPicker.Items.Add(port));
+            }));
+        }
+
+        private void TimeChanged(object sender, EventArgs e)
+        {
+            m_TimeSetManually = true;
+        }
+
+        private void SaveConfigurationClicked(object sender, EventArgs e)
+        {
+            string port = m_PortPicker.Text;
+            if (string.IsNullOrWhiteSpace(port))
+            {
+                MessageBox.Show("No port selected");
+                return;
+            }
+            DateTime time = m_DatePicker.Value.Date + m_TimePicker.Value.TimeOfDay;
+
+            Task.Factory.StartNew(() => SaveConfiguration(port, time));
+        }
+
+        private void SaveConfiguration(string portName, DateTime time)
+        {
+            try
+            {
+                using (SerialPort port = new SerialPort(portName))
+                {
+
+                    port.Open();
+                    port.WriteTimeout = 1000;
+                    string timeString = time.ToString("yyyy,MM,dd,HH,mm,ss");
+                    port.WriteLine(timeString);
+                    port.Close();
+                }
+                Invoke(new MethodInvoker(() =>
+                {
+                    if (Disposing || IsDisposed)
+                        return;
+                    MessageBox.Show("Configuration saved");
+                }));
+            }
+            catch (Exception ex)
+            {
+                Invoke(new MethodInvoker(() =>
+                {
+                    if (Disposing || IsDisposed)
+                        return;
+                    MessageBox.Show("Error saving configuration: " + ex.Message);
+                }));
+            }
+        }
+    }
+}
